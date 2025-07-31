ba31ba20ab6dd1a32e86a9cbd37dabbfcb408528
Switch-modder <deolrajjyot@gmail.com>
2025-07-31 17:47:13 -0400

    Mount data as exec and allow for proddev otas

--- poky/meta-anki/recipes-core/initscript-anki/files/init-boot.sh
+++ poky/meta-anki/recipes-core/initscript-anki/files/init-boot.sh
@@ -31,13 +31,13 @@ echo 1 > /sys/kernel/debug/regulator/8916_l17/enable
 echo 1 > /sys/kernel/debug/regulator/8916_l4/enable
 
 # Power on hardware test and led states
-if [ -z "${CMDLINE##*anki.dev*}" ]; then
+#if [ -z "${CMDLINE##*anki.dev*}" ]; then
 	is_dev_device=true
-	rampost syscon.dfu -d | tee /dev/rampost.log
-else
-	is_dev_device=false
+#	rampost syscon.dfu -d | tee /dev/rampost.log
+#else
+#	is_dev_device=false
 	rampost syscon.dfu | tee /dev/rampost.log
-fi
+#fi
 
 if [ -z "${CMDLINE##*dm=*}" ]; then
 	DM="${CMDLINE##*dm=\"}"
